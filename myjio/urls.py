923cab6592b971b7240bf88d7afe98eeef107425
barotraj2909 <rajbarot932847@gmail.com>
2023-04-18 23:59:36 +0530

    18.04.2023

--- myjio/urls.py
+++ myjio/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('contact/',views.contact,name='contact'),
     path('signup/',views.signup,name='signup'),
     path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('change-password/',views.change_password,name='change-password'),
 ]
--- myjio/views.py
+++ myjio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Contact,User
 # Create your views here.
 def index(request):
@@ -22,19 +22,67 @@ def contact(request):
 
 def signup(request):
 	if request.method=="POST":
-		User.objects.create(
-				fname=request.POST['fname'],
-				lname=request.POST['lname'],
-				email=request.POST['email'],
-				mobile=request.POST['mobile'],
-				gender=request.POST['gender'],
-				address=request.POST['address'],
-				password=request.POST['password']
-			)
-		msg="User sign up Successfully"
-		return render(request,'signup.html',{'msg':msg})
+		try:
+			user=User.objects.get(email=request.POST['email'])
+			msg="Email Already Registered"
+			return render(request,'signup.html',{'msg':msg})
+		except:
+			if request.POST['password']==request.POST['cpassword']:
+				User.objects.create(
+						fname=request.POST['fname'],
+						lname=request.POST['lname'],
+						email=request.POST['email'],
+						mobile=request.POST['mobile'],
+						gender=request.POST['gender'],
+						address=request.POST['address'],
+						password=request.POST['password']
+					)
+				msg="User sign up Successfully"
+				return render(request,'signup.html',{'msg':msg})
+			else:
+				msg="Password & Confirm Password Does Not Matched"
+				return render(request,'signup.html',{'msg':msg})
 	else:
 		return render(request,'signup.html')
 
 def login(request):
+	if request.method=="POST":
+		try:
+			user=User.objects.get(email=request.POST['email'])
+			if user.password==request.POST['password']:
+				request.session['email']=user.email
+				request.session['fname']=user.email
+				return render(request,'index.html')
+			else:
+				msg="Invalid Password"
+				return render(request,'login.html',{'msg':msg})
+		except:
+			msg="Email Not Registered"
+			return render(request,'login.html',{'msg':msg})
 	return render(request,'login.html')
+
+def logout(request):
+	try:
+		del request.session['email']
+		del request.session['fname']
+		return render(request,'login.html')
+	except:
+		return render(request,'login.html')
+
+def change_password(request):
+	if request.method=="POST":
+		user=User.objects.get(email=request.session['email'])
+		if user.password==request.POST['old_password']:
+			if request.POST['new_password']==request.POST['cnew_password']:
+				user.password=request.POST['new_password']
+				user.save()
+				return redirect('logout')
+			else:
+				msg="New Password & Confirm New Password Does Not Matched"
+				return render(request,'change-password.html',{'msg':msg})
+		else:
+			msg="Old Password Does Not Matched"
+			return render(request,'change-password.html',{'msg':msg})			
+
+	else:
+		return render(request,'change-password.html')
